fc017e4c4559a9b2736883b5eedaf5b520e8a2e2
JenHLee <hj.jennie.lee@gmail.com>
2022-05-24 11:41:32 -0600

    add like btn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
+/* eslint-disable */
+
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  let post = 'Docker for DevOps';
-  let [title, b] = useState('What is AWS?');
-
+  //let post = 'Docker for DevOps';
+  let [title, setTitle] = useState(['What is React.js?', 'Why React.js is important?', 'What is Components?']);
+  let [like0, setLike0] = useState(0);
+  let [like1, setLike1] = useState(0);
+  let [like2, setLike2] = useState(0);
   return (
     <div className="App">
       <div className="black-nav">
-        <h3>Blog</h3>
+        <h3>React Simple Blog</h3>
       </div>
+
       <div className="list">
-        <h4>{title}</h4>
+        <h4>
+          {title[0]}<span onClick={() => { setLike0(like0 + 1) }}>🤍</span> {like0} </h4>
         <p>May 20, 2022</p>
+
+        <h4>{title[1]} <span onClick={() => { setLike1(like1 + 1) }}>🤍</span> {like1} </h4>
+        <p>May 21, 2022</p>
+
+        <h4>{title[2]} <span onClick={() => { setLike2(like2 + 1) }}>🤍</span> {like2} </h4>
+        <p>May 22, 2022</p>
       </div>
-    </div>
+      <Modal></Modal>
+    </div >
   );
 }
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>Title</h4>
+      <p>Date</p>
+      <p>Datail</p>
+    </div>
+  )
+}
+
 
 export default App;
